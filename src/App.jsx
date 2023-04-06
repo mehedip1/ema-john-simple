25e59d5265cffe0c0ddd9858938d41e320a03ac7
mehedip1 <Promehedi30@gmail.com>
2023-04-06 22:08:07 +0600

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <div className="App">
-     <Header></Header>
-     <Shop></Shop>
+     
+    <Shop></Shop>
     </div>
   )
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Login come-ing soon</h3>
+        </div>
+    );
+};
+
+export default Login;
